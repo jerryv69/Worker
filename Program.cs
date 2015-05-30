a3567e5244efa46ebb4efc3d05ebc3bf5065fc20
jerryv69 <jerry_texas2009@hotmail.com>
2015-05-30 10:05:58 -0500

    Test
    Test

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System.IO;
+using System;
+
+namespace Worker {
+    class Program {
+        private static readonly ISchedulerFactory SchedulerFactory;
+        private static readonly IScheduler Scheduler;
+        private static IJobDetail _emailJobDetail;
+
+        static Program() {
+
+            // Create a regular old Quartz scheduler
+            SchedulerFactory = new StdSchedulerFactory();
+            Scheduler = SchedulerFactory.GetScheduler();
+
+        }
+
+        static void Main(string[] args) {
+            
+            // Now let's start our scheduler; you could perform
+            // any processing or bootstrapping code here before
+            // you start it but it must be started to schedule
+            // any jobs
+            Scheduler.Start();
+
+            // Let's generate our email job detail now
+            CreateJob();
+        }
+
+        private static void CreateJob() {
+
+            // The job builder uses a fluent interface to
+            // make it easier to build and generate an
+            // IJobDetail object
+            _emailJobDetail = JobBuilder.Create<EmailJob>()
+                .WithIdentity("SendToMyself")   // Here we can assign a friendly name to our job        
+                .Build();                       // And now we build the job detail
+
+        }
+    }
+
+    /// <summary>
+    /// Our email job, yet to be implemented
+    /// </summary>
+    public class EmailJob : IJob {
+        public void Execute(IJobExecutionContext context) {
+
+            // Let's start simple, write to the console
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
