0b9c9597abd58d7e2428683bef7cb5061f6810f5
jerryv69 <jerry_texas2009@hotmail.com>
2015-05-30 22:08:12 -0500

    test

--- Program.cs
+++ Program.cs
@@ -83,7 +83,7 @@ namespace Worker {
         public void Execute(IJobExecutionContext context) {
 
             // Let's start simple, write to the console
-            Console.WriteLine("Hello World! " + DateTime.Now.ToString("h:mm:ss tt"));
+            Console.WriteLine("Hello World 60! " + DateTime.Now.ToString("h:mm:ss tt"));
         }
     }
 }
